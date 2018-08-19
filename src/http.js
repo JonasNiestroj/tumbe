97387c474adaf8184955206d5c2f438b5e3298f8
Jonas <jonas.n@t-online.de>
2018-08-19 17:32:54 +0200

    Changed response object to object with more information

--- src/http.js
+++ src/http.js
@@ -19,20 +19,46 @@ function buildURLFromParams(url, params){
     return url
 }
 
+function buildResponse(xmlhttp){
+    var response = {}
+    response.data = xmlhttp.response
+    response.status = xmlhttp.status
+    response.statusText = xmlhttp.statusText
+    response.headers = {}
+    var headersText = xmlhttp.getAllResponseHeaders()
+    var headers = headersText.split('\n')
+    for(var i = 0; i < headers.length; i++){
+        var headerSplit = headers[i].split(': ')
+        if(headerSplit[0] !== ""){
+            response.headers[headerSplit[0]] = headerSplit[1]
+        }
+
+        // Check for content-type json
+        if(headerSplit[0].toLowerCase() === 'content-type'){
+            if(headerSplit[1].indexOf('application/json') !== -1){
+                // Parse data to object
+                response.data = JSON.parse(response.data)
+            }
+        }
+    }
+
+    return response
+}
+
 function get(url, options){
     return new Promise(function(resolve, reject){
         var xmlhttp = new XMLHttpRequest()
         xmlhttp.onload = function() {
             if(xmlhttp.status == 200){
-                resolve(xmlhttp.response)
+                resolve(buildResponse(xmlhttp))
             }
             else{
-                reject(xmlhttp.response)
+                reject(buildResponse(xmlhttp))
             }
         }
 
         xmlhttp.onerror = function(){
-            reject(xmlhttp.response)
+            reject(buildResponse(xmlhttp))
         }
 
         if(options.params){
@@ -49,15 +75,15 @@ function post(url, params){
         var xmlhttp = new XMLHttpRequest()
         xmlhttp.onload = function(){
             if(xmlhttp.status == 200){
-                resolve(xmlhttp.response)
+                resolve(buildResponse(xmlhttp))
             }
             else{
-                reject(xmlhttp.response)
+                reject(buildResponse(xmlhttp))
             }
         }
 
         xmlhttp.onerror = function(){
-            reject(xmlhttp.response)
+            reject(buildResponse(xmlhttp))
         }
 
         xmlhttp.open("POST", url, true)
